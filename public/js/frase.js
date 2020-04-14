839ddd241743c04373c24bc5a4c4a01a061da869
claudia-duda <claudiaeduarda_duda@hotmail.com>
2020-04-14 17:56:44 -0300

    spinner and erro

--- public/js/frase.js
+++ public/js/frase.js
@@ -2,8 +2,17 @@ $("#botao-frase").click(fraseAleatoria);
 
 
 function fraseAleatoria(){//pegando do servidor q o node roda com ajax
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
-    
+    $('#spinner').toggle();
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },1500);
+    })
+    .always(function(){
+        $('#spinner').toggle();
+    });
 }
 
 function trocaFraseAleatoria(data){
